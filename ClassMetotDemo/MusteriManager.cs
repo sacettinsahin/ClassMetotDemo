ec8af1e7a5a980c905709b9d1bc09f59bc5f96ee
casper <casper@DESKTOP-U7BT6RT>
2022-03-30 16:04:13 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Listeye eklenen müşteri : "+musteri.Ad +" " +musteri.Soyad);
+            Console.WriteLine("*****************************");
+        }
+        public void Cikar(Musteri musteri)
+        {
+            Console.WriteLine("Listeden çıkarılan müşteri : "+musteri.Ad +" "+ musteri.Soyad);
+            Console.WriteLine("*****************************");
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.ID + " ID'li Müşteri Bilgileri;");
+            Console.WriteLine("Müşteri ID : " +musteri.ID);
+            Console.WriteLine("Müşteri Adı : " +musteri.Ad);
+            Console.WriteLine("Müşteri Soyadı : " +musteri.Soyad);
+            Console.WriteLine("----------------------------");
+            
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.ID = 1;
+            musteri1.Ad = "Sacettin";
+            musteri1.Soyad = "ŞAHİN";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.ID = 2;
+            musteri2.Ad = "Ali";
+            musteri2.Soyad = "Kemal";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.ID = 3;
+            musteri3.Ad = "Ayşe";
+            musteri3.Soyad = "Yıldız";
+
+            Musteri musteri4 = new Musteri();
+            musteri4.ID = 4;
+            musteri4.Ad = "Melih";
+            musteri4.Soyad = "Özdilek";
+
+            Musteri[] musteriler = new Musteri[]{musteri1,musteri2,musteri3,musteri4};
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri ID : " +musteri.ID);
+                Console.WriteLine("Müşteri Adı : " +musteri.Ad);
+                Console.WriteLine("Müşteri Soyadı : " +musteri.Soyad);
+                Console.WriteLine("---------------------");
+            }
+
+            MusteriManager musteriIslem = new MusteriManager();
+            musteriIslem.Ekle(musteri1);
+            musteriIslem.Cikar(musteri2);
+            musteriIslem.Listele(musteri3);
+            musteriIslem.Listele(musteri4);
+        }
+    }
+}
